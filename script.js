005bb1698bba248035e4367b069d3295ee1711b3
PITSTOP08 <162000782+PITSTOP08@users.noreply.github.com>
2026-05-17 18:49:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+// TYPING
+
+const text = "Happy 1st Monthsary mylovee!❤️";
+
+    const typing = document.getElementById("typing");
+
+    let i = 0;
+
+    function typeText() {
+
+        if (i < text.length) {
+
+            typing.textContent += text.charAt(i);
+
+            i++;
+
+            setTimeout(typeText, 100);
+
+        } else {
+
+            // Wait before restarting
+            setTimeout(() => {
+
+                typing.textContent = "";
+                i = 0;
+                typeText();
+
+            }, 2000);
+
+        }
+
+    }
+
+    typeText();
+
+
+
+
+// BUTTON
+
+document
+.getElementById("btn")
+
+.addEventListener(
+"click",
+
+()=>{
+
+document
+.querySelector(".memories")
+
+.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+
+
+
+// FALLING HEARTS
+
+function createHeart(){
+
+const heart=
+document
+.createElement("div");
+
+heart.className=
+"heart";
+
+heart.innerHTML=
+"❤️";
+
+heart.style.left=
+Math.random()*100
++"vw";
+
+
+heart.style.fontSize=
+(Math.random()*20+10)
++"px";
+
+
+heart.style.animationDuration=
+(Math.random()*3+2)
++"s";
+
+
+document.body
+.appendChild(heart);
+
+
+setTimeout(()=>{
+
+heart.remove();
+
+},5000);
+
+}
+
+setInterval(
+createHeart,
+300
+);
+
+
+
+
+// MUSIC
+
+const music=
+document.getElementById(
+"bgMusic"
+);
+
+
+music.play().catch(()=>{
+
+document
+.addEventListener(
+"click",
+
+()=>{
+
+music.play();
+
+},
+
+{once:true}
+
+);
+
+});
+
+});
+
+
+// FINAL BUTTON ACTION
+
+const loveBtn =
+document.querySelector(".love-btn");
+
+loveBtn.addEventListener(
+"click",
+
+()=>{
+
+alert(
+"I love you more than anything mylovee❤️"
+);
+
+// after OK is clicked
+window.location.href = "sunflower.html";
+
+});
